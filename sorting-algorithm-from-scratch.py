d9abf1417dd1e455ffedba471751ffa75a4eb670
silvercrown27 <bradabwao14@gmail.com>
2023-02-28 15:01:00 +0300

    first commit

--- sorting-algorithm-from-scratch.py
+++ sorting-algorithm-from-scratch.py
@@ -1,18 +1,23 @@
 from random import randint
-
-numbers = [randint(0, 1000000) for i in range(1000)]
+#  Generate 1000 random numbers between 0 and 100000
+numbers = [randint(0, 100000) for i in range(1000)]
+#  create an empty list to store the sorted values
 sorted = []
 
 
 def sort_func(nums):
+    #  create an empty dictionary to store each unique value
+    #  and the number of times it occurs
     values = {}
+    #  store the values in a set to get each unique value
     data = set(nums)
+    #  Getting the number of times a value occurs
     for i in data:
-        occurence = 0
+        occurrence = 0
         for k in nums:
             if i == k:
-                occurence += 1
-        values[i] = occurence
+                occurrence += 1
+        values[i] = occurrence
     values_copy = [i for i in values.keys()]
 
     for n in nums:
